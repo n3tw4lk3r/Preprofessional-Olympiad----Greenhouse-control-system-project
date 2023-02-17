616838cd8404c47e7b6d22649ce5707ac99428c6
Pavel <75166974+PavelKhod@users.noreply.github.com>
2023-02-17 18:41:07 +0300

    Новая версия main.js

--- greenhouse-main/main.js
+++ greenhouse-main/main.js
@@ -1,10 +1,6 @@
 // Не забыть добавить токен в заголовок всех запросов
 // axios.defaults.headers.common["X-Auth-Token"] = "<token>"
 
-let T = null;
-let H = null;
-let Hb = null;
-
 // данные с датчиков
 let air_temp = [null, null, null, null];
 let air_hum = [null, null, null, null];
@@ -14,27 +10,53 @@ let soil_hum = [null, null, null, null, null, null];
 let watering = null;
 let fork_drive = null;
 let soil_watering = [null, null, null, null, null, null];
+let emergencyMode = false;
+
+// параметры
+let T = null;
+let H = null;
+let Hb = null;
+
+// последние показания датчиков (используется очередь)
+let queue_lastAirHum = [
+    ["?", "?", "?", "?", "?", "?", "?", "?", "?", "?"],
+    ["?", "?", "?", "?", "?", "?", "?", "?", "?", "?"],
+    ["?", "?", "?", "?", "?", "?", "?", "?", "?", "?"],
+    ["?", "?", "?", "?", "?", "?", "?", "?", "?", "?"]
+];
+let queue_lastAirTemp = [
+    ["?", "?", "?", "?", "?", "?", "?", "?", "?", "?"],
+    ["?", "?", "?", "?", "?", "?", "?", "?", "?", "?"],
+    ["?", "?", "?", "?", "?", "?", "?", "?", "?", "?"],
+    ["?", "?", "?", "?", "?", "?", "?", "?", "?", "?"]
+];
+let queue_lastSoilHum = [
+    ["?", "?", "?", "?", "?", "?", "?", "?", "?", "?"],
+    ["?", "?", "?", "?", "?", "?", "?", "?", "?", "?"],
+    ["?", "?", "?", "?", "?", "?", "?", "?", "?", "?"],
+    ["?", "?", "?", "?", "?", "?", "?", "?", "?", "?"],
+    ["?", "?", "?", "?", "?", "?", "?", "?", "?", "?"],
+    ["?", "?", "?", "?", "?", "?", "?", "?", "?", "?"]
+];
+let queue_lastAverageAirHum = ["?", "?", "?", "?", "?", "?", "?", "?", "?", "?"];
+let queue_lastAverageAirTemp = ["?", "?", "?", "?", "?", "?", "?", "?", "?", "?"];
+let queue_lastAverageSoilHum = [
+    ["?", "?", "?", "?", "?", "?", "?", "?", "?", "?"],
+    ["?", "?", "?", "?", "?", "?", "?", "?", "?", "?"],
+    ["?", "?", "?", "?", "?", "?", "?", "?", "?", "?"],
+    ["?", "?", "?", "?", "?", "?", "?", "?", "?", "?"],
+    ["?", "?", "?", "?", "?", "?", "?", "?", "?", "?"],
+    ["?", "?", "?", "?", "?", "?", "?", "?", "?", "?"]
+];
 
 // средние значения
+let average_air_temp = null;
+let average_air_hum = null;
 
-// т.к. по ТЗ хотят видеть динамику изменения средней температуры и влажности,
-// считаю средние данные "в моменте", как среднее от данных всех датчиков
-// влажности или температуры в момент запроса 
-
-let temp_average = null;
-let hum_average = null;
-
-// для каждой бороздки почвы средняя влажность - сумма всех значений влажности,
-// разделённая на общее кол-во запросов
-
-// сумма всех значений влажности почвы
-let soil_hum_total = [null, null, null, null, null, null];
-
-// средняя влажность
-let soil_hum_average = [null, null, null, null, null, null];
-
-// сколько раз запрашивались данные (кол-во вызовов функции getSensorData)
-let count = 0; 
+// считаю средние показатели влажности бороздок как среднее арифметическое
+let soil_hum_total = [null, null, null, null, null, null]; // сумма значений
+let count = 0; // кол-во сохранённых значений (кол-во вызовов функции getSensorData)
+let soil_hum_average = [null, null, null, null, null, null]; // итоговое среднее
 
 let url_air = "http://91.240.84.86:5000/get?sensor_type=temp_hum&sensor_id=";
 let url_soil = "http://91.240.84.86:5000/get?sensor_type=hum&sensor_id=";
@@ -43,47 +65,93 @@ let url_watering_open = "http://91.240.84.86:5000/patch?target=watering&state=1&
 let url_watering_close = "http://91.240.84.86:5000/patch?target=watering&state=0&id=";
 let url_totalHum = "http://91.240.84.86:5000/patch?target=total_hum&state=";
 
+
+function switchTest() {
+    alert(document.getElementById('switch').value);
+}
+
 function patchRequest(url, target, state, id) {
-    if (target == watering) {
+    // path-запрос к API организаторов
+    // на самом деле делает get-запрос к бэкенду, а бэкенд уже делает get
+    if (target == "watering") {
         axios.get(url+id.toString())
         .then(response => {
             alert(JSON.stringify(response.data));
         })
     }
-    else {
+    else if (target == "fork_drive") {
         axios.get(url+state.toString())
         .then(response => {
-            alert(JSON.stringify(response.data));
+            if (JSON.stringify(response.data).split(' ')[5] == 'open"}') {
+                document.getElementById("currentForkDrive").innerHTML = "Форточки открыты";
+            }
+            else {
+                document.getElementById("currentForkDrive").innerHTML = "Форточки закрыты";
+            }
+        })
+    }
+    else if (target == "total_hum") {
+        axios.get(url+state.toString())
+        .then(response => {
+            if (JSON.stringify(response.data).split(' ')[5] == 'start"}') {
+                document.getElementById("currentWatering").innerHTML = "Общее увлажнение включено";
+            }
+            else {
+                document.getElementById("currentWatering").innerHTML = "Общее увлажнение отключено";
+            }
         })
     }
 }
 
-function setParam(name, param, data) {
-    param = data;
-    alert(name + " теперь " + param);
+function setParameter(parameter, data) {
+    if (parameter == "T") {
+        T = data;
+        document.getElementById("currentT").innerHTML = "Параметр T = " + T.toString();
+    }
+    else if (parameter == "H") {
+        H = data;
+        document.getElementById("currentH").innerHTML = "Параметр H = " + H.toString();
+    }
+    else {
+        Hb = data;
+        document.getElementById("currentHb").innerHTML = "Параметр Hb = " + Hb.toString();
+    }
 }
 
 function getSensorData() {
-    // функция была вызвана ещё раз
+    // функция была вызвана ещё раз (учитываем при подсчёте средней влажности)
     count++;
-    // получаем данные с каждого датчика, сохраняем в массивы air_temp и air_hum
+    // получаем и сохраняем данные с каждого датчика
     for (let air_sensor_id = 0; air_sensor_id < 4; air_sensor_id++) {
         axios.get(url_air + (air_sensor_id + 1).toString())
             .then(response => {
                 air_temp[air_sensor_id] = response.data.temperature;
+                queue_lastAirTemp[air_sensor_id].push(response.data.temperature);
+                queue_lastAirTemp[air_sensor_id].shift();
                 air_hum[air_sensor_id] = response.data.humidity;
+                queue_lastAirHum[air_sensor_id].push(response.data.humidity);
+                queue_lastAirHum[air_sensor_id].shift();
+                
             })
     }
 
-    temp_average = (air_temp[0] + air_temp[1] + air_temp[2] + air_temp[3]) / 4;
-    hum_average = (air_hum[0] + air_hum[1] + air_hum[2] + air_hum[3]) / 4;
+    average_air_temp = ((air_temp[0] + air_temp[1] + air_temp[2] + air_temp[3]) / 4).toFixed(2);
+    queue_lastAverageAirTemp.push(average_air_temp);
+    queue_lastAverageAirTemp.shift();
+    average_air_hum = ((air_hum[0] + air_hum[1] + air_hum[2] + air_hum[3]) / 4).toFixed(2);
+    queue_lastAverageAirHum.push(average_air_hum);
+    queue_lastAverageAirHum.shift();
 
     for (let soil_sensor_id = 0; soil_sensor_id < 6; soil_sensor_id++) {
         axios.get(url_soil + (soil_sensor_id + 1).toString())
             .then(response => {
                 soil_hum[soil_sensor_id] = response.data.humidity;
+                queue_lastSoilHum[soil_sensor_id].push(response.data.humidity);
+                queue_lastSoilHum[soil_sensor_id].shift();
                 soil_hum_total[soil_sensor_id] += response.data.humidity;
-                soil_hum_average[soil_sensor_id] = soil_hum_total[soil_sensor_id] / count;
+                soil_hum_average[soil_sensor_id] = (soil_hum_total[soil_sensor_id] / count).toFixed(2);
+                queue_lastAverageSoilHum[soil_sensor_id].push(soil_hum_average[soil_sensor_id]);
+                queue_lastAverageSoilHum[soil_sensor_id].shift();
             })
     }
 
@@ -112,6 +180,8 @@ function createChart_air(divId, data_hum, data_temp) {
     );
 }
 
+
+
 function updateChart_soil(divId, data_hum) {
     Plotly.extendTraces(divId, {
         y: [
@@ -131,11 +201,10 @@ function updateChart_air(divId, data_hum, data_temp) {
 
 // получаем первые данные с датчиков
 getSensorData();
-
 // регулярно запрашиваем новые
 setInterval(async () => {
     getSensorData()
-}, 200);
+}, 1000);
 
 // создаём графики
 for (let soil_sensor_id = 0; soil_sensor_id < 6; soil_sensor_id++) {
@@ -148,83 +217,117 @@ for (let air_sensor_id = 0; air_sensor_id < 4; air_sensor_id++) {
         air_hum[air_sensor_id], air_temp[air_sensor_id]
     );
 }
+Plotly.plot('chart_average_soil',
+    [{
+        y: [queue_lastAverageSoilHum[0][-1]],
+        type: 'line',
+        name: "Бороздка 1",
+    },
+    {
+        y: [queue_lastAverageSoilHum[1][-1]],
+        type: 'line',
+        name: "Бороздка 2",
+    },
+    {
+        y: [queue_lastAverageSoilHum[2][-1]],
+        type: 'line',
+        name: "Бороздка 3",
+    },
+    {
+        y: [queue_lastAverageSoilHum[3][-1]],
+        type: 'line',
+        name: "Бороздка 4",
+    },
+    {
+        y: [queue_lastAverageSoilHum[4][-1]],
+        type: 'line',
+        name: "Бороздка 5",
+    },
+    {
+        y: [queue_lastAverageSoilHum[5][-1]],
+        type: 'line',
+        name: "Бороздка 6",
+    }
+    ],
+);
+createChart_air('chart_average_air',
+        queue_lastAverageAirHum[-1], queue_lastAverageAirTemp[-1]
+    );
 
-// обновляем графики с учётом новых данных каждые N секунд
-
-// ------------------------------------------------------------------------------
-// код ниже "красивый", но он не работает... Придётся использовать "грязный"
-/*
+// обновляем графики и таблицу с учётом новых данных каждые N секунд
 for (let soil_sensor_id = 0; soil_sensor_id < 6; soil_sensor_id++) {
-    setInterval(updateChart_soil('chart_soil_' + (soil_sensor_id + 1).toString(),
-            soil_hum[soil_sensor_id]), 200);
+    let idChart = 'chart_soil_' + (soil_sensor_id + 1).toString();
+    let idTable = 'lastSoilHum' + (soil_sensor_id + 1).toString();
+    setInterval(function() {
+        Plotly.extendTraces(idChart, {
+            y: [
+                [soil_hum[soil_sensor_id]]
+            ]
+        }, [0]);
+        document.getElementById(idTable).innerHTML = queue_lastSoilHum[soil_sensor_id].join(' ');
+    }, 1000);
 }
 for (let air_sensor_id = 0; air_sensor_id < 4; air_sensor_id++) {
-    setInterval(updateChart_air('chart_air_' + (air_sensor_id + 1).toString(),
-            air_hum[air_sensor_id], air_temp[air_sensor_id]), 200);
+    let idChart= 'chart_air_' + (air_sensor_id + 1).toString();
+    let idTableTemp = 'lastAirTemp' + (air_sensor_id + 1).toString();
+    let idTableHum = 'lastAirHum' + (air_sensor_id + 1).toString();
+    setInterval(function() {
+        Plotly.extendTraces(idChart, {
+            y: [[air_hum[air_sensor_id]], [air_temp[air_sensor_id]]]
+        }, [0, 1]);
+        document.getElementById(idTableTemp).innerHTML = queue_lastAirTemp[air_sensor_id].join(' ');
+        document.getElementById(idTableHum).innerHTML = queue_lastAirHum[air_sensor_id].join(' ');
+        document.getElementById('lastAverageAirTemp').innerHTML = queue_lastAverageAirTemp.join(' ');
+        document.getElementById('lastAverageAirHum').innerHTML = queue_lastAverageAirHum.join(' ');
+    }, 1000);
 }
-*/
-// ------------------------------------------------------------------------------
 
 setInterval(function() {
-    Plotly.extendTraces('chart_soil_1', {
-        y: [
-            [soil_hum[0]]
-        ]
-    }, [0]);
-}, 200);
-setInterval(function() {
-    Plotly.extendTraces('chart_soil_2', {
-        y: [
-            [soil_hum[1]]
-        ]
-    }, [0]);
-}, 200);
-setInterval(function() {
-    Plotly.extendTraces('chart_soil_3', {
-        y: [
-            [soil_hum[2]]
-        ]
-    }, [0]);
-}, 200);
-setInterval(function() {
-    Plotly.extendTraces('chart_soil_4', {
-        y: [
-            [soil_hum[3]]
-        ]
-    }, [0]);
-}, 200);
-setInterval(function() {
-    Plotly.extendTraces('chart_soil_5', {
-        y: [
-            [soil_hum[4]]
-        ]
-    }, [0]);
-}, 200);
-setInterval(function() {
-    Plotly.extendTraces('chart_soil_6', {
-        y: [
-            [soil_hum[5]]
-        ]
-    }, [0]);
-}, 200);
+    let data = '';
+    for (let i=0; i<6; i++) {
+        data = data + queue_lastAverageSoilHum[i].join(' ') + "<br>";
+    }
+    document.getElementById('lastAverageSoilHum').innerHTML = data;
+}, 1000);
+
 
 setInterval(function() {
-    Plotly.extendTraces('chart_air_1', {
-        y: [[air_hum[0]], [air_temp[0]]]
+    Plotly.extendTraces('chart_average_air', {
+        y: [
+            [queue_lastAverageAirHum[9]],
+            [queue_lastAverageAirTemp[9]]
+        ]
     }, [0, 1]);
-}, 200);
+}, 1000);
+
+
 setInterval(function() {
-    Plotly.extendTraces('chart_air_2', {
-        y: [[air_hum[1]], [air_temp[1]]]
-    }, [0, 1]);
-}, 200);
-setInterval(function() {
-    Plotly.extendTraces('chart_air_3', {
-        y: [[air_hum[2]], [air_temp[2]]]
-    }, [0, 1]);
-}, 200);
-setInterval(function() {
-    Plotly.extendTraces('chart_air_4', {
-        y: [[air_hum[3]], [air_temp[3]]]
-    }, [0, 1]);
-}, 200);
+    Plotly.extendTraces('chart_average_soil', {
+        y: [
+            [queue_lastAverageSoilHum[0][9]],
+            [queue_lastAverageSoilHum[1][9]],
+            [queue_lastAverageSoilHum[2][9]],
+            [queue_lastAverageSoilHum[3][9]],
+            [queue_lastAverageSoilHum[4][9]],
+            [queue_lastAverageSoilHum[5][9]],
+        ]
+    }, [0, 1, 2, 3, 4, 5]);
+}, 1000);
+
+document.querySelector(".emergencyModeSwitch").addEventListener("change", function(){
+    emergencyMode = emergencyMode ? false : true;
+    
+})
+
+for (let i=0; i<6; i++) {
+    classId = ".soilWateringSwitch" + (i+1).toString();
+    document.querySelector(classId).addEventListener("change", function(){
+        soil_watering[i] = soil_watering[i] ? false : true;
+        if (soil_watering[i]) {
+            patchRequest(url_watering_open, 'watering', 1, i+1);
+        }
+        else {
+            patchRequest(url_watering_close, 'watering', 1, i+1);
+        }
+    })
+}
